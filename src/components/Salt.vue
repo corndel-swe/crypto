b0e37c51c0346eba8ec4e8ea042246a4fb90190d
Simon Haines <nimon@hotmail.co.uk>
2024-08-07 15:28:23 +0100

    adds salting

---
+++ src/components/Salt.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { reactive } from 'vue'
+import md5 from 'md5'
+
+const state = reactive({
+  salt: '',
+  plaintext: '',
+  hashtext: null
+})
+
+function hashIt() {
+  state.hashtext = md5(state.salt + state.plaintext)
+}
+</script>
+
+<template>
+  <h1>MD5 Salt + Hash</h1>
+  <form @submit.prevent="hashIt()">
+    <input
+      type="text"
+      id="salt"
+      v-model="state.salt"
+      placeholder="salt"
+      autocomplete="off"
+    />
+    <input
+      type="text"
+      id="plaintext"
+      v-model="state.plaintext"
+      placeholder="input text"
+      autocomplete="off"
+    />
+    <input type="submit" value="Hash it" />
+  </form>
+  <p>{{ state.hashtext }}</p>
+</template>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
 import Hash from '../components/Hash.vue'
+import Salt from '../components/Salt.vue'
 import Encrypt from '../components/Encrypt.vue'
 import Decrypt from '../components/Decrypt.vue'
 
 const routes = [
   { path: '/hash', component: Hash },
+  { path: '/salt', component: Salt },
   { path: '/encrypt', component: Encrypt },
   { path: '/decrypt', component: Decrypt }
 ]
